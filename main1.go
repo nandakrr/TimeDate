1e56b4821e3ac77bd201d769697049c5fa115645
Nanda_Kumar <42697805+nandakrr@users.noreply.github.com>
2021-12-10 22:38:22 +0530

    Add files via upload

---
+++ main1.go
@@ -0,0 +1,18 @@
+// Golang program to get the current date and time
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, time.Now())
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
+}
